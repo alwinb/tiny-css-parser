d44c20f8a99db6d15c2c0a39eab117113797b30f
Alwin Blok <alwinb@users.noreply.github.com>
2018-03-21 14:00:53 +0100

    first run of the new lexer

--- src/grammar.js
+++ src/grammar.js
@@ -1,57 +1,88 @@
 "use strict"
+const tinylex = require ('./tiny-lexer')
+var log = console.log.bind (console)
 
-//
-//  A tiny-lexer based tokenizer for CSS
+// A tiny-lexer based tokenizer for CSS
+// ====================================
+
+// TODO list:
+// - [x] handle surrogate pairs (not needed)
+// - [ ] unicode range tokens
+// - [ ] count newlines
+// - [ ] hashid vs hash?
+// - [ ] url parsing (via coalescing)
+// - [ ] coalescing (see below)
+// - [x] sqstring (via this.quot)
+// - [x] operators, |=, ~= etc
+
+// ### Regular expressions
+// These are used in the tiny-lexer grammar below.
+// Each regular expression corresponds to a transition between states. 
 
 const R_space = '[\t ]+'
-const R_newline = '[\n\r\f]'
-const R_url = '[Uu][Rr][Ll]\\(' // May do that on 
-const R_lgroup = '\\(\\[\\{'
-const R_rgroup = '\\)\\]\\}'
-//const R_op = /// TODO
+const R_newline = '\r?\n|[\r\f]'
+const R_lgroup = '[({[]'
+const R_rgroup = '[)}\\]]'
+const R_op = '[|~^*$]='
+
 
 // Strings
 const R_dqstring = '[^\n\r\f\\"]+'
 const R_sqstring = "[^\n\r\f\\']+"
+const R_string = "[^\n\r\f\\'\"]+"
 const R_hex_esc = '\\\\[0-9A-Fa-f]{1,6}[ \t\n\r\f]?' // NB newlines
-const R_nl_esc = '\\\\[\n\r\f]'
+const R_nl_esc = '\\\\[\n\r\f]' // NB newlines
 
 // Numbers
 const R_fract = '(?:\\.[0-9]+)'
-const R_opt_exp = '(?:[eE][+-]?[0-9]+)?'
+const R_opt_exp = '(?:[eE][+\\-]?[0-9]+)?'
 const R_number = '[+-]?(?:' + R_fract + '|[0-9]+' + R_fract + '?)' + R_opt_exp
 
 // Names
-// I am doing a lookahead and emit an empty T_name_start token, switch to name. 
-const R_name_start = '.{0}(?=\\-?(:?[A-Za-z_\u0080-\uFFFF]|\\\\[^\n\r\f]))'
-const R_name = '\\[A-Za-z_\u0080-\uFFFF0-9\\-]+'
+// I am doing a lookahead, emit an empty T_name_start token and switch to name. 
+const R_name_start = '.{0}(?=\\-?(?:[A-Za-z_\u0080-\uFFFF]|\\\\[^\n\r\f]))'
+const R_name = '[A-Za-z0-9\\-_\u0080-\uFFFF]+'
 
-// TODO list:
-// [ ] handle surrogate pairs
-// [ ] unicode range tokens
-// [ ] count newlines
-// [ ] hashid vs hash?
-// [ ] url parsing
-// [ ] coalescing
-// [ ] sqstring
 
-// Coalescing
-// <number><ident> => <dimension>
-// <number>% => <percentage>
-// #<name> => <hash>
-// @<name> => <at-keyword>
+// ### Token types
 
-// Parsing
-// <ident><lparen> => <func-start>
-// ...
+let t = 0
+const T_CDC = 'CDC' // ++t
+  , T_CDO = 'CDO' // ++t
+  , T_at = 'at' // ++t
+  , T_badstring_end = 'badstring-end' // ++t
+  , T_comment_data = 'comment-data' // ++t
+  , T_comment_end = 'comment-end' // ++t
+  , T_comment_start = 'comment-start' // ++t
+  , T_delim = 'delim' // ++t
+  , T_delim_invalid = 'delim-invalid' // ++t
+  , T_escape_char = 'escape-char' // ++t
+  , T_escape_eof = 'escape-eof' // ++t
+  , T_escape_hex = 'escape-hex' // ++t
+  , T_group_end = 'group-end' // ++t
+  , T_group_start = 'group-start' // ++t
+  , T_hash = 'hash' // ++t
+  , T_ignore_newline = 'ignore-newline' // ++t
+  , T_name_data = 'name-data' // ++t
+  , T_name_end = 'name-end' // ++t
+  , T_name_start = 'name-start' // ++t
+  , T_newline = 'newline' // ++t
+  , T_number = 'number' // ++t
+  , T_op = 'operator' // ++t
+  , T_column = 'column' // ++t
+  , T_sep = 'sep' // ++t
+  , T_space = 'space' // ++t
+  , T_string_data = 'string-data' // ++t
+  , T_string_end = 'string-end' // ++t
+  , T_string_start = 'string-start' // ++t
 
+
+// ### The actual grammar
 
 const grammar = 
 { main: [
   { if: '/\\*',       emit: T_comment_start,  goto: 'comment'   },
-  { if: '"',          emit: T_string_start,   goto: 'dqstring'  },
-  { if: "'",          emit: T_string_start,   goto: 'sqstring'  },
-  { if: R_url,        emit: T_url_start,      goto: 'url'       },
+  { if: '"|\'',       emit: start_string,     goto: 'string'    },
   { if: R_name_start, emit: T_name_start,     goto: 'name'      },
   { if: R_space,      emit: T_space,                            },
   { if: R_newline,    emit: T_newline,                          },  // NB newlines
@@ -62,6 +93,7 @@ const grammar =
   { if: '@',          emit: T_at,                               },
   { if: '[,:;]',      emit: T_sep,                              }, // In the spec these are separate tokens
   { if: R_op,         emit: T_op,                               }, // likewise
+  { if: '[|][|]',     emit: T_column,                           },
   { if: '<!--',       emit: T_CDO,                              },
   { if: '-->',        emit: T_CDC,                              },
   { if: '\\\\',       emit: T_delim_invalid,                    },
@@ -69,15 +101,15 @@ const grammar =
   ]
 
 , comment: [
-  { if: '\\*/',       emit: T_comment_end,    goto: 'main'      },
+  { if: '[*]/',       emit: T_comment_end,    goto: 'main'      },
+  { if: '[*][^/][^*]*',emit: T_comment_data                     },
   { if: '[^*]+',      emit: T_comment_data,                     },
-  { if: '*[^/][^*]*', emit: T_comment_data                      },
   ]
 
-, dqstring: [ // TODO add sqstring
+, string: [
   { if: '[\n\r\f]',   emit: T_badstring_end,  goto: 'main'      }, // NB newlines
-  { if: '"',          emit: T_string_end,     goto: 'main'      },
-  { if: R_dqstring,   emit: T_string_data                       },
+  { if: '"|\'',       emit: quote_emit,       goto: quote_state },
+  { if: R_string,     emit: T_string_data                       },
   { if: R_nl_esc,     emit: T_ignore_newline                    },
   { if: '\\\\$',      emit: T_escape_eof                        },
   { if: R_hex_esc,    emit: T_escape_hex                        },
@@ -85,12 +117,55 @@ const grammar =
   ]
 
 , name: [
-  { if: R_name,       emit: T_name_data   },
-  { if: R_hex_esc,    emit: T_escape_hex  },
-  { if: '\\\\.',      emit: T_escape_char }, // FIXME what about backslash-newline?
-  { emit: T_name_end, goto: 'main' },
+  { if: R_name,       emit: T_name_data                         },
+  { if: R_hex_esc,    emit: T_escape_hex                        },
+  { if: '\\\\.',      emit: T_escape_char                       }, // FIXME what about backslash-newline?
+  {                   emit: T_name_end,       goto: 'main'      },                           
   ]
+}
 
+// Additional state management, to
+//  supplement the grammar/ state machine
+
+function start_string (chunk) {
+  this.quot = chunk
+  return [T_string_start, chunk]
+}
+
+function quote_emit (chunk) {
+  return [chunk === this.quot ? T_string_end : T_string_data, chunk]
+}
+
+function quote_state (chunk) {
+  return chunk === this.quot ? 'main' : 'string'
+}
+
+
+//
+// Test
+
+var sample = require ('fs') .readFileSync ( __dirname+'/../test/style6.dpl')
+var sample = ' testtin "a string with \' a bad end " uaa'
+const chunks = new tinylex (grammar, 'main', {}, sample)
+
+for (var i of chunks)
+  log (i)
+
+
+// coalescing, may even make it into a simple parser then. 
+// <number><ident> => <dimension>
+// <number>% => <percentage>
+// #<name> => <hash>
+// @<name> => <at-keyword>
+// <ident><lparen> => <func-start>
+// <name:url><lparen> => <url-start>
+// <name-start><name-data><name-end> ==> <name>
+// <string-start><string-data>*<badstring-end> ==> ??
+
+function* coalesced (tokens) {
+  let spare
+  for (let token of tokens) 
+    null // TODO
 }
 
 
---
+++ src/tiny-lexer.js
@@ -0,0 +1,104 @@
+"use strict"
+module.exports = Lexer
+var log = console.log.bind (console)
+
+// Tiny lexer runtime
+// ==================
+
+// The idea is that lexical grammars can be very compactly expressed by a 
+// state machine that has transitions that are labeled with regular expressions
+// rather than individual characters. 
+
+// Since the javascript RegExp object allows reading and writing the position
+// of reference in the input string, it is possible to represent each state by
+// a single RegExp object with capture groups; each capture groups then 
+// identifies a transition. 
+
+// The Lexer runtime
+// -----------------
+
+function Lexer (grammar, start, customState, input) {
+  const states = compile (grammar)
+	const self = this
+
+	let symbol = start
+    , position = 0
+
+  this.goto = function name (s) { symbol = s }
+
+  this.value
+  this.done = false
+  this.next = next
+  this[Symbol.iterator] = _ => this // That should create a copy instead, or be up one level
+
+	function next () {
+    if (states [symbol] == null) 
+      throw new Error ('Lexer: no such symbol: '+symbol)
+
+    let state = states [symbol]
+      , regex = state.regex
+
+    regex.lastIndex = position
+		let r = regex.exec (input)
+    // log (position, r)
+
+    if (position === input.length && regex.lastIndex === 0) {
+      self.value = null
+      self.done = true
+			return self
+    }
+
+		if (r == null) { // FIXME end at EOF only, else emit error? or throw it?
+      log ('invalid')
+      self.value = '' // Or even specify that explicitly in the grammar?
+      self.done = true
+			return self
+    }
+
+    // TODO take care not to skip chars! (e.g. simulate the y flag)..
+    // log (position, regex.lastIndex)
+		position = regex.lastIndex
+    let i = 1; while (r[i] == null) i++
+    let edge = state.edges [i-1]
+      , chunk = r[i]
+
+
+    self.value = (typeof edge.emit === 'function')
+      ? edge.emit.call (customState, chunk) // output token
+      : [edge.emit, chunk]
+
+    symbol = (typeof edge.goto === 'function')
+      ? edge.goto.call (customState, chunk)
+      : edge.goto
+
+    return self
+	}
+}
+
+
+// The compiler
+// ------------
+
+function State (table, name) {
+	this.regex = new RegExp ('(' + table.map (fst) .join (')|(') + ')', 'gy')
+	this.edges = table.map ( fn )
+
+  function fn (row) {
+    return { goto:'goto' in row ? row.goto : name, emit: 'emit' in row ? row.emit : null }
+  }
+}
+
+function fst (row) {
+  return ('if' in row) ? row ['if'] : '.{0}'
+}
+
+function compile (grammar) {
+	const compiled = {}
+	for (let state_name in grammar)
+		compiled [state_name] = new State (grammar [state_name], state_name)
+	return compiled
+}
+
+
+
+
